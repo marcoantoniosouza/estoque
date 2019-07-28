e2943f38eaaa88d5ecc2b1b5e9a1307d3ad14ea6
Marco Antonio Souza <maps@ic.ufal.br>
2019-07-28 18:20:13 -0300

    API Controller and Swagger

---
+++ src/main/java/com/estoque/produtos/resources/ProdutoApiResource.java
@@ -0,0 +1,50 @@
+package com.estoque.produtos.resources;
+
+import com.estoque.produtos.models.Produto;
+import com.estoque.produtos.repository.ProdutoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+@Api(value = "API Produtos")
+@CrossOrigin(origins = "*")
+public class ProdutoApiResource {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    @GetMapping("/produtos")
+    @ApiOperation(value = "Retorna uma lista de produtos do estoque")
+    public List<Produto> listarProdutos(){
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping("/produtos/{id}")
+    @ApiOperation(value = "Retorna um produto do estoque")
+    public Produto listaProduto(@PathVariable(value = "id") long id) {
+        return produtoRepository.findById(id);
+    }
+
+    @PostMapping("/produtos")
+    @ApiOperation(value = "Insere um produto")
+    public Produto salvaProduto(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    @DeleteMapping("/produtos")
+    @ApiOperation(value = "Deleta um produto")
+    public void deletaProduto(@RequestBody Produto produto){
+        produtoRepository.deleteById(produto.getId());
+    }
+
+    @PutMapping("/produtos")
+    @ApiOperation(value = "Atualiza um produto")
+    public Produto atualizaProduto(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
+    }
+}
