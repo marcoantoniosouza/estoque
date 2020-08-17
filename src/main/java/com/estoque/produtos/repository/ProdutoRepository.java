d6ec9ae5db4d7fe874147ad0efc16508c79bcf4a
Marco Antonio Souza <maps@ic.ufal.br>
2020-08-16 22:01:38 -0300

    change ProdutoRepository

--- src/main/java/com/estoque/produtos/repository/ProdutoRepository.java
+++ src/main/java/com/estoque/produtos/repository/ProdutoRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
-    Produto findById(long id);
+    <List>Produto findByNome(String nome);
 
 }
